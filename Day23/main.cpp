f2f52a2f06eebfba247422f65d3d779509a848f9
alexdcdc <69087503+alexdcdc@users.noreply.github.com>
2024-12-23 01:06:26 -0500

    start day 23

---
+++ Day23/main.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <regex>
+
+void part1() {
+    std::ifstream inputFile("DayX/input.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Unable to open the file." << std::endl;
+        return;
+    }
+}
+
+void part2() {
+    std::ifstream inputFile("DayX/input.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Unable to open the file." << std::endl;
+        return;
+    }
+}
+
+
+int main() {
+    part1();
+    return 0;
+}
